11b29743aa15374f2026a1172d702c231485a214
Colin Newman <goldenofficer17@colinn.dev>
2021-01-15 16:26:06 -0600

    Adding Reset Password Functionality to Login Screen for Users, Untested.

--- index.php
+++ index.php
@@ -55,9 +55,87 @@ if(!file_exists(getcwd().'/oc-config.php') && is_writable(getcwd())){
 	  $loginMessage = '<div class="alert alert-danger" style="text-align: center;"><span>'.$_SESSION['loginMessageDanger'].'</span></div>';
 		unset($_SESSION['loginMessageDanger']);
 	}
+	if(isset($_POST["email"]) && (!empty($_POST["email"]))){
+    $email = $_POST["email"];
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+    if (!$email) {
+        $error ="<p>Invalid email address please type a valid email address!</p>";
+    }else{
+        $sel_query = "SELECT * FROM ".DB_PREFIX."users WHERE email='".$email."'";
+        $con = PDO::prepare("INSERT INTO ".DB_PREFIX."password_reset_temp (`email`, `key`, `expDate`)
+VALUES ('?', '?', '?');");
+        $results = mysqli_query($con, $sel_query);
+        $row = mysqli_num_rows($results);
+        if ($row==""){
+            $error = "<p>No user is registered with this email address!</p>";
+        }
+    }
+    if($error!=""){
+        echo "<div class='error'>".$error."</div>
+   <br /><a href='javascript:history.go(-1)'>Go Back</a>";
+    }else{
+        $expFormat = mktime(
+            date("H"), date("i"), date("s"), date("m") ,date("d")+1, date("Y")
+        );
+        $expDate = date("Y-m-d H:i:s",$expFormat);
+        $key = md5(2418*2+$email);
+        $addKey = substr(md5(uniqid(rand(),1)),3,10);
+        $key = $key . $addKey;
+// Insert Temp Table
+        mysqli_query($con,
+            "INSERT INTO ".DB_PREFIX."password_reset_temp (`email`, `key`, `expDate`)
+VALUES ('".$email."', '".$key."', '".$expDate."');");
 
+        $output='<p>Dear user,</p>';
+        $output.='<p>Please click on the following link to reset your password.</p>';
+        $output.='<p>-------------------------------------------------------------</p>';
+        $output.='<p><a href="https://'.$_SERVER['SERVER_NAME'].'/reset-password.php?
+key='.$key.'&email='.$email.'&action=reset" target="_blank">
+https://'.$_SERVER['SERVER_NAME'].'/reset-password.php
+?key='.$key.'&email='.$email.'&action=reset</a></p>';
+        $output.='<p>-------------------------------------------------------------</p>';
+        $output.='<p>Please be sure to copy the entire link into your browser.
+The link will expire after 1 day for security reason.</p>';
+        $output.='<p>If you did not request this forgotten password email, no action 
+is needed, your password will not be reset. However, you may want to log into 
+your account and change your security password as someone may have guessed it.</p>';
+        $output.='<p>Thanks,</p>';
+        $output.=  '<COMMUNITY_NAME>';
+        $body = $output;
+        $subject = "Password Recovery - <COMMUNITY_NAME>";
 
+        $email_to = $email;
+        $fromserver = "noreply@".BASE_URL."";
+        require("PHPMailer/PHPMailerAutoload.php");
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->Host = "mail.".BASE_URL.""; // Enter your host here
+        $mail->SMTPAuth = true;
+        $mail->Username = "noreply@".BASE_URL.""; // Enter your email here
+        $mail->Password = "password"; //Enter your password here
+        $mail->Port = 25;
+        $mail->IsHTML(true);
+        $mail->From = "noreply@".BASE_URL."";
+        $mail->FromName = "<COMMUNITY_NAME>";
+        $mail->Sender = $fromserver; // indicates ReturnPath header
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AddAddress($email_to);
+        if(!$mail->Send()){
+            echo "Mailer Error: " . $mail->ErrorInfo;
+        }else{
+            echo "<div class='error'>
+<p>An email has been sent to you with instructions on how to reset your password.</p>
+</div><br /><br /><br />";
+        }
+    }
+}else{
 ?>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<?php } ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -81,7 +159,12 @@ if(!file_exists(getcwd().'/oc-config.php') && is_writable(getcwd())){
 					 </div>
 					 <div>
 						<input name="login_btn" type="submit" class="btn btn-default submit" value="Login" />
-						<a class="reset_pass" href="#" onclick="alert('Request an administrator reset your password through your community.');" >Lost your password?</a>
+						<a <form method="post" action="" name="reset"><br /><br />
+                            <label><strong>Enter Your Email Address:</strong></label><br /><br />
+                            <input type="email" name="email" placeholder="username@email.com" />
+                            <br /><br />
+                            <input type="submit" value="Reset Password"/>
+                  </form> Lost your password?</a>
 					 </div>
 					 <?php if (CIV_REG === true) { ?>
 			 <div class="clearfix"></div>
